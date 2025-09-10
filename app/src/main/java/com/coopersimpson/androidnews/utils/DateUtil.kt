c0bb733111ecabd2c5dcf9f1672b026c06a85f11
Cooper Simpson <cooper@coopersimpson.com>
2025-09-11 08:12:06 +1000

    Display article pubDate in local time

--- app/src/main/java/com/coopersimpson/androidnews/utils/DateUtil.kt
+++ app/src/main/java/com/coopersimpson/androidnews/utils/DateUtil.kt
@@ -1,15 +1,20 @@
 package com.coopersimpson.androidnews.utils
 
-import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 fun convertDate(input: String): String {
-    val apiFormat = "yyyy-MM-dd HH:mm:ss"
-    val desiredFormat = "MMM d, h:mm a"
+    val apiFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.getDefault())  // Incoming UTC format
+    val desiredFormat = DateTimeFormatter.ofPattern("MMM d, h:mm a", Locale.getDefault()) // Output in local format
 
-    val parser = SimpleDateFormat(apiFormat)
-    val printer = SimpleDateFormat(desiredFormat)
+    val utcDateTime = LocalDateTime.parse(input, apiFormat)
+        .plusHours(12); // Add 12 hrs cause API is 12 hours behind real UTC time ?
+    val zonedUtc = ZonedDateTime.of(utcDateTime, ZoneId.of("UTC"));
 
-    val parsed = parser.parse(input) ?: return input
+    val localZoned = zonedUtc.withZoneSameInstant(ZoneId.systemDefault())
 
-    return printer.format(parsed)
+    return desiredFormat.format(localZoned)
 }
